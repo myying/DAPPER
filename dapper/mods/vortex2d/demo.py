d61bb5866f8f3f0b009f623eda29f46e6d0d6bb6
Yue Ying <hardyying@gmail.com>
2021-03-07 20:58:53 +0100

    fix demo plots

--- dapper/mods/vortex2d/demo.py
+++ dapper/mods/vortex2d/demo.py
@@ -9,24 +9,24 @@ import dapper.tools.viz as viz
 
 def show(x, model, ax=None):
     u, v, q, p = model.expand(x)
-    zeta = vortex2d.uv2zeta(u, v, model.dx)
-    im = ax.imshow(zeta)
+    wind = vortex2d.uv2wspd(u, v)
+    im = ax.imshow(wind, cmap='plasma')
     def update(x):
         u, v, q, p = model.expand(x)
-        zeta = vortex2d.uv2zeta(u, v, model.dx)
-        im.set_data(zeta)
+        wind = vortex2d.uv2wspd(u, v)
+        im.set_data(wind)
         plt.pause(0.01)
     return update
-
-xx = np.load(vortex2d.sample_filename)['sample']
 
 fig = plt.figure(figsize=(8, 4))
 ax = plt.subplot(111)
 ax.set_aspect('equal', viz.adjustable_box_or_forced())
-ax.set_title('')
+ax.set_title('Wind speed (m/s)')
 
+xx = np.load(vortex2d.sample_filename)['sample']
+setter = show(xx[0], vortex2d.model_config(), ax=ax)
 for k, x in progbar(list(enumerate(xx)), "Animating"):
     if k%1 == 0:
-        show(x, vortex2d.model_config(), ax=ax)
-        fig.suptitle("k: "+str(k))
+        setter(x)
+        fig.suptitle("time step: "+str(k))
         plt.pause(0.01)
